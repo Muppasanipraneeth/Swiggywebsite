7d998e826b6b9146a309cddd832939e18b302970
praneeth <muppasanipraneeth19@gmail.com>
2024-10-18 13:06:42 +0530

    changed the files

--- swiggyapp/src/components/Restaurantcard.jsx
+++
@@ -1,8 +0,0 @@
-import { Res_url } from "../utils/logo";
-const Restaurantcard=(props)=>{
-    const {resData}=props;
-    return (<>
-    <div> this is praneeth</div>
-    </>);
-}
-export default Restaurantcard;
--- swiggyapp/src/utils/Usercontext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-const Usercontext = createContext({
-    username: 'praneeth'
-});
-
-export default Usercontext;
